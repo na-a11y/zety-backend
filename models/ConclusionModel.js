9a7269476ec08ff648275cab18adb264f65656bb
na-a11y <navjotmanak01@gmail.com>
2024-11-20 19:27:56 +0530

    zety-backend

---
+++ models/ConclusionModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// Define the schema for a conclusion
+const conclusionSchema = new mongoose.Schema({
+  conclusion: {
+    type: String,
+    required: true 
+  },
+});
+
+// Export the model to use it in other parts of the application
+module.exports = mongoose.model('Conclusion', conclusionSchema);
---
+++ models/paymentModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const paymentSchema = new mongoose.Schema({
+  cardNumber: String,
+  cardHolder: String,
+  expiry: String,
+  cvv: String,
+  plan: String,
+  amount: String,
+});
+
+module.exports = mongoose.model('Payment', paymentSchema);
